a90cb2f4577ed6809c6dfd3e47416f06c8ca2501
JamesLijx <lijxmail@sina.cn>
2021-01-10 15:34:18 +0800

    一点小小的变动而已

--- git01/src/main/java/com/lijxStudy/HelloJavaBranch.java
+++ git01/src/main/java/com/lijxStudy/HelloJavaBranch.java
@@ -3,5 +3,7 @@ package com.lijxStudy;
 public class HelloJavaBranch {
     public static void main(String[] args) {
         System.out.println("This is the branch class");
+        System.out.println("This is a new branch class");
+        System.out.println("This is a second branch class");
     }
 }
